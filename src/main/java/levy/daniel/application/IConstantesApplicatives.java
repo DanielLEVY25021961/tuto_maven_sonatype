003ad9789d95bbf26a31de80fbbed7f3500092df
DanielLevy25021961 <dany.levy@free.fr>
2017-10-06 13:51:14 +0200

    COMMIT_DL_DOMICILE après IConstantesApplicatives

---
+++ src/main/java/levy/daniel/application/IConstantesApplicatives.java
@@ -0,0 +1,37 @@
+package levy.daniel.application;
+
+
+
+/**
+ * class IConstantesApplicatives :<br/>
+ * <ul>
+ * <li>INTERFACE contenant les constantes mises à disposition 
+ * de l'ensemble de l'application.</li>
+ * </ul>
+ *
+ * - Exemple d'utilisation :<br/>
+ * <code>// Appel depuis n'importe quelle classe de l'application pour concaténer le point-virgule au StringBuilder stb : </code><br/>
+ * <code>stb.append(IConstantesApplicatives.POINT_VIRGULE);</code><br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author dan Lévy
+ * @version 1.0
+ * @since 6 oct. 2017
+ *
+ */
+public interface IConstantesApplicatives {
+	
+	/**
+	 * POINT_VIRGULE : char :<br/>
+	 * ';'.<br/>
+	 */
+	char POINT_VIRGULE = ';';
+
+} // FIN DE L'INTERFACE IConstantesApplicatives.-----------------------------

1254ee63fc3d5e75cd6707dd5146edb4677e223d
DanielLevy25021961 <dany.levy@free.fr>
2017-10-06 17:19:55 +0200

    COMMIT_DL_DOMICILE

--- src/main/java/levy/daniel/application/model/services/metier/meteo/YahooMeteoRetriever.java
+++ src/main/java/levy/daniel/application/model/services/metier/meteo/YahooMeteoRetriever.java
@@ -35,8 +35,13 @@ public final class YahooMeteoRetriever {
 	
 	/**
 	 * URL_QUERY_METEO_YAHOO : String :<br/>
-	 * URL de base du Web Service Météo de Yahoo.<br/>
-	 * "https://query.yahooapis.com/v1/public/yql?q=".<br/>
+	 * <ul>
+	 * <li>URL de base du Web Service Météo de Yahoo.</li>
+	 * <li>"https://query.yahooapis.com/v1/public/yql?q=".</li>
+	 * <li>La valeur du paramètre q est la requête YQL précisant 
+	 * le lieu pour lequel ont veut obtenir la météo ainsi que 
+	 * divers attributs.</li>
+	 * </ul>
 	 */
 	public static final String URL_QUERY_METEO_YAHOO 
 		= "https://query.yahooapis.com/v1/public/yql?q=";
